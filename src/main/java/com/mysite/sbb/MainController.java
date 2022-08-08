95d890c46c09b8263ccb60ebb7ab7b39e1c49868
jpark6 <qkstjr09@gmail.com>
2022-08-08 17:49:20 +0900

    Feat: Question & Answer

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -1,16 +1,20 @@
 package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+@GetMapping("/")
+public String index() {
+  return "redirect:/question/list";
+}
 
-    @RequestMapping("/sbb")
-    @ResponseBody
-    public String index() {
-        return "안녕하세요 sbb에 오신 것을 환영합니다.";
-    }
+@GetMapping("/sbb")
+@ResponseBody
+public String sbb() {
+  return "안녕하세요 sbb에 오신 것을 환영합니다.";
+}
 }
 
--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,14 +1,34 @@
 package com.mysite.sbb.question;
 
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequestMapping("/question")
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
-  @RequestMapping("/question/list")
-  @ResponseBody
-  public String list() {
-    return "question list";
+  private final QuestionService questionService;
+
+  @GetMapping("/list")
+  public String list(Model model) {
+    List<Question> questionList = this.questionService.getList();
+    model.addAttribute("questionList", questionList);
+    return "question_list";
+  }
+  @GetMapping("/detail/{id}")
+  public String detail(Model model, @PathVariable("id") Integer id) {
+    Question question = this.questionService.getQuestion(id);
+    model.addAttribute("question", question);
+    return "question_detail";
+  }
+
+  @GetMapping("/create")
+  public String questionCreate() {
+    return "question_form";
   }
 }
